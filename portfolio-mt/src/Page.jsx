39c738ceac26d85b6267177b9e1241328c8f4409
mtliche <tlichemohamed76@gmail.com>
2025-02-09 17:19:49 +0100

    v2 supabase

--- portfolio-mt/src/Page.jsx
+++ portfolio-mt/src/Page.jsx
@@ -3,8 +3,29 @@ import Projet from "./components/Projet.jsx";
 import Techno from "./components/Techno.jsx";
 import Contact from "./components/Contact.jsx";
 import Desc from "./components/Desc.jsx";
+import {useEffect, useState} from "react";
+import supabase from "../utils/supabase.js";
+
 
 function Page() {
+    const  [myProjects , setMyProject] = useState([])
+    useEffect(() => {
+        const getData = async () => {
+            const { data: project, error } = await supabase
+                .from('project')
+                .select('*');
+
+            if (error) {
+                console.error("Erreur lors de la récupération des données :", error);
+                return;
+            }
+
+            if (project && project.length > 1) {
+                setMyProject(project);
+            }
+        };
+        getData();
+    }, []);
     return (
         <div>
            <Header/>
@@ -15,7 +36,9 @@ function Page() {
                     <h2 className="text-2xl font-bold mb-4">Projets</h2>
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                         {/* Projet terminé */}
-                        <Projet/>
+                        {myProjects.map((projet, index) => (
+                            <Projet key={index} projet={projet} />
+                        ))}
                     </div>
                 </section>
                 {/* Section Technologies maîtrisées */}
--- portfolio-mt/src/components/Projet.jsx
+++ portfolio-mt/src/components/Projet.jsx
@@ -1,13 +1,13 @@
-function Projet() {
+function Projet( { projet }) {
     return (
         <>
             <div className="card bg-base-100 shadow-xl">
                 <div className="card-body">
                     <div className="flex items-center justify-between">
-                        <h3 className="card-title">Projet Terminé</h3>
+                        <h3 className="card-title">{projet.title}</h3>
                         <div className="w-3 h-3 rounded-full bg-green-500" title="Projet terminé"></div>
                     </div>
-                    <p>Voici une courte description du projet terminé qui présente ses principales fonctionnalités.</p>
+                    <p>{projet.description}</p>
                     <div className="mt-2">
                         <span className="badge badge-secondary mr-2">React</span>
                         <span className="badge badge-secondary mr-2">Node.js</span>
